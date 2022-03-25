b65404febdbf56d323846e304412847a02570e11
Kim Dongwoo <andandend2847@gmail.com>
2022-03-25 17:57:49 +0900

    refactor: users/serializers.py 주석 제거

--- user_demo/users/serializers.py
+++ user_demo/users/serializers.py
@@ -31,11 +31,6 @@ class UserDetailSerializer(serializers.ModelSerializer):
 
 
 class UserLoginSerializer(serializers.Serializer):
-    """
-    (이메일, 전화번호)중 하나와 (비밀번호, 전화번호 인증 키)중 하나를 이용해 로그인이 가능한지 확인합니다.
-    로그인이 가능하면 user를 반환합니다.
-    """
-
     email = serializers.EmailField(required=False)
     phone_number = serializers.CharField(required=False)
     key = serializers.IntegerField(required=False, write_only=True)
@@ -46,13 +41,6 @@ class UserLoginSerializer(serializers.Serializer):
 
 
 class UserFindPasswordSerializer(serializers.Serializer):
-    """
-    이메일, 변경하고자 하는 비밀번호, 전화번호 인증 토큰을 요청받아,
-    토큰이 유효하면 해당 유저의 비밀번호를 변경하고자 하는 비밀번호로 변경합니다.
-
-    시리얼라이저 validation을 진행하면 모든 로직이 진행되고 user를 반환합니다.
-    """
-
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
     token = serializers.CharField(write_only=True)
